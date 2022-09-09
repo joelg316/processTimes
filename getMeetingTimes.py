286ce67d1bb93236f346f0525724964e6d2291c3
Jelo <13345755+joelg316@users.noreply.github.com>
2022-09-09 11:58:39 -0500

    Ported script to getMeetingTimes.py to query the cloud flow directly instead of using Power Automate Desktop app, since that app does not run on Macs. Also converted it to python3 since that is the default version on Macs and I was only using python2 because it's required by Power Automate Desktop.

--- getMeetingTimes.py
+++ getMeetingTimes.py
@@ -1,12 +1,12 @@
+#!python3
 # A script to process and format available meeting times in Outlook
 # Version 1.0 8/22/22
 
+import requests
+import json
 from datetime import datetime, timedelta
 
 # input data is retrieved from Power Automate "Get meeting times" cloud flow via HTTP request
-input = %WebServiceResponse%
-
-# test input data
 # input = ["2022-08-23T08:00:00.0000000", "2022-08-25T11:00:00.0000000", "2022-08-26T11:30:00.0000000",
 #        "2022-08-29T08:00:00.0000000", "2022-08-29T12:00:00.0000000", "2022-08-31T11:30:00.0000000",
 #        "2022-08-23T12:00:00.0000000", "2022-08-25T15:00:00.0000000", "2022-08-26T15:30:00.0000000",
@@ -19,10 +19,21 @@ input = %WebServiceResponse%
 # 8/29:  8 - 4 CST
 # 8/31: 12 - 3 CST
 
+# Settings to query power automate cloud flow HTTP listener URL
+cloud_url = 'https://prod-34.westus.logic.azure.com:443/workflows/0b0f0bd35618401f86477a7a830f1dac/triggers/manual/paths/invoke?api-version=2016-06-01&sp=/triggers/manual/run&sv=1.0&sig=gtV0akNrZOZic_icEBuOWGSzvtcWn8CdMH_IgShHyhY'
+headers = {'Content-Type': 'application/json'}
+# http request body below must be wrapped in ''' to avoid error for invalid characters like the comma
+# modify the email and duration as needed
+body = '''{'email':'joel_ginsberg@trendmicro.com','duration':240}'''
+
+# make HTTP request and store response in r
+r = requests.post(cloud_url, headers=headers, data=body)
+# parse json data from HTTP response content and assign it to input array
+input = json.loads(r.content)
+print(input)
 
 # sort times chronologically
 input.sort()
-
 
 # combine consecutive meeting blocks into one to make output more intuitive
 # the times list contains both start and end times in chronological order, evens are start times and odds are end times
@@ -31,7 +42,6 @@ input.sort()
 # we have to call the function recursively until all consecutive meeting blocks have been combined
 # all consecutive meeting blocks have been combined if the function runs and does not remove any elements from the list
 def combineTimes(times):
-
     # get initial length of times list to compare to length after running this function
     # this is how we check if we can stop calling the function recursively
     beforeLength = len(times)
@@ -40,7 +50,7 @@ def combineTimes(times):
         # avoid error on last element
         if i < (len(times) - 1):
             # check if current datetime is the same as the next one
-            #print input[i], input[i+1]
+            # print input[i], input[i+1]
             if times[i] == times[i + 1]:
                 # after the first pop the next element [i+1] becomes the current one [i]
                 # we want to remove both duplicates
@@ -55,7 +65,8 @@ def combineTimes(times):
         return combineTimes(times)  # call function again until all consecutive times have been combined
 
 input = combineTimes(input)
-#print input
+
+# print(input)
 
 
 # Change :30 times to :00. To avoid conflict with other meetings, add 30 min to start times and subtract 30 from end times
@@ -64,20 +75,21 @@ def shrinkby30(times):
     # add 30 min to start times
     for i, e in enumerate(times[0::2]):  # check every two elements in list starting with first element
         if ":30" in e:
-            dt = datetime.strptime(e, '%%Y-%%m-%%dT%%H:%%M:%%S.0000000')   # convert string to datetime
+            dt = datetime.strptime(e, '%Y-%m-%dT%H:%M:%S.0000000')  # convert string to datetime
             dt = dt + timedelta(minutes=30)  # add 30 min
             # multiply i * 2 because enumerate only got every other element from times so it's half as long
-            times[i * 2] = dt.strftime('%%Y-%%m-%%dT%%H:%%M:%%S.0000000')  # format dt as string again
+            times[i * 2] = dt.strftime('%Y-%m-%dT%H:%M:%S.0000000')  # format dt as string again
 
     # subtract 30 min from end times
     for i, e in enumerate(times[1::2]):  # check every two elements in list starting with second element
         if ":30" in e:
-            dt = datetime.strptime(e, '%%Y-%%m-%%dT%%H:%%M:%%S.0000000')
+            dt = datetime.strptime(e, '%Y-%m-%dT%H:%M:%S.0000000')
             dt = dt - timedelta(minutes=30)  # subtract 30 min
             # multiply i * 2 + 1 bc enumerate got every other element from times starting with position 1 not 0
-            times[i * 2 + 1] = dt.strftime('%%Y-%%m-%%dT%%H:%%M:%%S.0000000')
+            times[i * 2 + 1] = dt.strftime('%Y-%m-%dT%H:%M:%S.0000000')
 
     return times
+
 
 input = shrinkby30(input)
 
@@ -86,10 +98,12 @@ input = shrinkby30(input)
 output = []
 for x, y in zip(input[0::2], input[1::2]):
     # convert strings x and y to datetime objects a and b so they can be formatted for human readable output
-    a = datetime.strptime(x, '%%Y-%%m-%%dT%%H:%%M:%%S.0000000')
-    b = datetime.strptime(y, '%%Y-%%m-%%dT%%H:%%M:%%S.0000000')
+    a = datetime.strptime(x, '%Y-%m-%dT%H:%M:%S.0000000')
+    b = datetime.strptime(y, '%Y-%m-%dT%H:%M:%S.0000000')
     # format list with human readable day of the week, month/day, and times plus time zone
-    # dt.strftime("%%A %%m/%%d %%I:%%M%%p %%Z")
-    output.append(a.strftime("%%m/%%d:").lstrip('0').replace('/0', '/') + a.strftime(" %%I").replace(' 0', ' ').rjust(3, ' ') + ' - ' + b.strftime("%%I CST").lstrip('0'))
+    # dt.strftime("%m/%d %I:%M%p %Z")
+    # I hard-coded CST instead of printing the timezone with %Z because the datetime objects returned from the cloud flow do not have a timezone value.
+    #output.append(a.strftime("%m/%d:").lstrip('0').replace('/0', '/') + a.strftime(" %I").replace(' 0', ' ').rjust(3, ' ') + ' - ' + b.strftime("%I CST").lstrip('0'))
+    output.append(a.strftime("%m/%d:").lstrip('0').replace('/0', '/') + a.strftime(" %I %p").replace(' 0', ' ').lower() + ' - ' + b.strftime("%I %p").lstrip('0').lower() + " CST")
 
-print '\n'.join(output)
+print('\n'.join(output))
